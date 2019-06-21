136fb82cabeb94a252c76858017f3ba751cd1885
cvacva <769978880@qq.com>
2019-06-21 13:59:42 +0800

    第一次提交代码

---
+++ src/views/anchor/components/rightBody/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我是右侧
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/anchor/index.vue
+++ src/views/anchor/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-    我是主持人首页
+  <div class="anchor" style="height:100%;">
+    <div style="height:40px;">
+      <oheader />
+    </div>
+    <div class="bigBox">
+      <div class="box" style="width:300px;">
+        <leftBody />
+      </div>
+      <div class="box" style="flex:1;">
+        <centerBody />
+      </div>
+      <div class="box" style="width:300px;">
+        <rightBody />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import oheader from './components/header'
+import leftBody from './components/leftBody'
+import rightBody from './components/rightBody'
+import centerBody from './components/centerBody'
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: { oheader, centerBody, rightBody, leftBody }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .bigBox {
+    display: flex;
+    height: calc(100vh - 40px);
+  }
 </style>
