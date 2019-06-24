e2f7e3bb1364f1272b9a77feecd827619a9592bd
cvacva <769978880@qq.com>
2019-06-24 09:41:05 +0800

    6465

--- src/views/anchor/index.vue
+++ src/views/anchor/index.vue
@@ -4,13 +4,15 @@
       <oheader />
     </div>
     <div class="bigBox" style="margin-top:15px;">
-      <div class="box" style="width:300px;background-color:#fff;margin:0 10px;">
+      <div class="box" style="position:relative;background-color:#fff;margin:0 10px;" :style="{width:leftW+'px'}">
+        <div style="position:absolute;right:-15px;top:0;bottom:0;width:15px;cursor:e-resize;" @mousedown="leftMove" />
         <leftBody />
       </div>
       <div class="box" style="flex:1;background-color:#fff;margin:0 5px;padding:0 10px;">
         <centerBody />
       </div>
-      <div class="box" style="width:300px;background-color:#fff;margin:0 10px;">
+      <div class="box" style="position:relative;background-color:#fff;margin:0 10px;" :style="{width:rightW+'px'}">
+        <div style="position:absolute;left:-15px;top:0;bottom:0;width:15px;cursor:e-resize;" @mousedown="rightMove" />
         <rightBody />
       </div>
     </div>
@@ -22,9 +24,64 @@ import oheader from './components/header'
 import leftBody from './components/leftBody'
 import rightBody from './components/rightBody'
 import centerBody from './components/centerBody'
+
 export default {
   name: 'Index',
-  components: { oheader, centerBody, rightBody, leftBody }
+  components: { oheader, centerBody, rightBody, leftBody },
+  data() {
+    return {
+      leftW: 300,
+      rightW: 300
+    }
+  },
+  methods: {
+    leftMove(ev) {
+      const event = ev || window.event
+      const disX = event.clientX
+      const _this = this
+      const initX = this.leftW
+      document.onmousemove = function(ev) {
+        const event = ev || window.event
+        const X = event.clientX
+        const l = X - disX
+        if (_this.leftW < 300) {
+          if (l >= 0) {
+            _this.leftW = initX + l
+          }
+        } else {
+          _this.leftW = initX + l
+        }
+        return false
+      }
+      document.onmouseup = function() {
+        document.onmousemove = null
+        document.onmouseup = null
+      }
+    },
+    rightMove(ev) {
+      const event = ev || window.event
+      const disX = event.clientX
+      const _this = this
+      const initX = this.rightW
+      document.onmousemove = function(ev) {
+        const event = ev || window.event
+        const X = event.clientX
+        const l = X - disX
+        if (_this.rightW < 300) {
+          if (l <= 0) {
+            _this.rightW = initX + (-l)
+          }
+        } else {
+          _this.rightW = initX + (-l)
+        }
+        return false
+      }
+      document.onmouseup = function() {
+        document.onmousemove = null
+        document.onmouseup = null
+      }
+    }
+  }
 }
 </script>
 
@@ -33,7 +90,8 @@ export default {
     display: flex;
     height: calc(100vh - 56px);
   }
-  .box{
+
+  .box {
     box-shadow: 0 0 15px #ddd;
   }
 </style>
